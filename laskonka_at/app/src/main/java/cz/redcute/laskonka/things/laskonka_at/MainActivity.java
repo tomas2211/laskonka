5a2b53cd0c54519081183c4f21692f096a4d6f03
jakobx <jakub.pogran@outboxers.com>
2018-07-22 19:08:31 +0200

    tlacitko

--- laskonka_at/app/src/main/java/cz/redcute/laskonka/things/laskonka_at/MainActivity.java
+++ laskonka_at/app/src/main/java/cz/redcute/laskonka/things/laskonka_at/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Handler;
 import android.util.Log;
 
 import com.google.android.things.pio.Gpio;
+import com.google.android.things.pio.GpioCallback;
 import com.google.android.things.pio.PeripheralManager;
 
 import java.io.IOException;
@@ -33,6 +34,8 @@ public class MainActivity extends Activity {
     private static final String TAG = "HomeActivity";
 
     private Gpio ledGpio;
+    private Gpio tlacitkoGpio;
+
 
     Handler timerHandler = new Handler();
     Runnable timerRunnable = new Runnable() {
@@ -46,7 +49,7 @@ public class MainActivity extends Activity {
             } catch (IOException e) {
                 Log.w(TAG, "Error using GPIO", e);
             }
-            Log.d(TAG, "Changed state of LED to: "+state);
+            Log.d(TAG, "Changed state of LED to: " + state);
             timerHandler.postDelayed(this, 500);
         }
     };
@@ -64,10 +67,52 @@ public class MainActivity extends Activity {
 
             ledGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
 
-            timerHandler.postDelayed(timerRunnable,500);
+            timerHandler.postDelayed(timerRunnable, 500);
         } catch (IOException e) {
             Log.w(TAG, "Error opening GPIO", e);
         }
 
     }
+
+
+    protected void listeningTlacitko(PeripheralManager pioManager) {
+        try {
+            tlacitkoGpio = pioManager.openGpio("BCM3");
+            tlacitkoGpio.setDirection(Gpio.DIRECTION_IN);
+
+            tlacitkoGpio.setEdgeTriggerType(Gpio.EDGE_BOTH);
+            tlacitkoGpio.registerGpioCallback(mGpioCallback);
+
+        } catch (IOException e) {
+            Log.w(TAG, "Error opening GPIO", e);
+        }
+
+        private GpioCallback mGpioCallback = new GpioCallback() {
+            @Override
+            public boolean onGpioEdge(Gpio gpio) {
+                // Read the active low pin state
+                try {
+                    if (tlacitkoGpio.getValue()) {
+
+                    } else {
+                        // Pin is LOW
+                    }
+                } catch (IOException e) {
+                    Log.w(TAG, tlacitkoGpio + "Err with tlacitko value");
+                }
+
+                // Continue listening for more interrupts
+                return true;
+            }
+
+            @Override
+            public void onGpioError(Gpio gpio, int error) {
+                Log.w(TAG, tlacitkoGpio + ": Error event " + error);
+            }
+        };
+
+
+    }
+
+
 }
